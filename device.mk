acb386239b07b1b98f672ed60249b3fe17a0f993
Killerpac <nilavasarkar@gmail.com>
2024-04-22 20:16:45 +0530

    nashc: Switch to prebuilt libwifi-hal
    * Reference: https://github.com/moto-common/android_device_mediatek_common/tree/master/vendor/wlan
    
    * Perhaps it will help with the Wi-Fi scanning issues,
      and intermittent disconnections we're having. Besides,
      we can't legally use the OSS one anyway.
    
      *libhal-mediatek is broken so using prebuild one
    
    Change-Id: I9bbf289ad88a12bbdc68ab0abdee410678bf1420
    Co-authored-by: electimon <electimon@gmail.com>
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -428,8 +428,7 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi-service-lazy \
     wpa_supplicant \
     hostapd \
-    libwifi-hal-wrapper \
-    libwifi-hal-mt66xx
+    libwifi-hal-wrapper
 
 # Tetheroffload
 PRODUCT_PACKAGES += \
