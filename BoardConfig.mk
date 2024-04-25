c3c7c6ff3313e9f722030efe4fe154a246e96dae
Killerpac <nilavasarkar@gmail.com>
2024-04-25 11:21:13 +0530

    nashc :Update BoardConfig.mk to remove duplicate sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,7 +153,6 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 
 # SELinux
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
