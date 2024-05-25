ab65a25183128b69b13fa0e78ce6eeffed71eac0
Killerpac <nilavasarkar@gmail.com>
2024-05-26 01:36:20 +0530

    nashc: fix cgroup

--- device.mk
+++ device.mk
@@ -347,7 +347,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.mt6785 \
     fstab.mt6785_ramdisk \
-    init.mt6785.power.rc \
+    init.cgroup.rc \
     init.connectivity.rc \
     init.modem.rc \
     init.mt6785.rc \
